08b5d01f079ad5dd509b404be093056387bb646b
Paul Parson <paul.parson@davidccook.com>
2014-05-22 15:19:02 -0600

    Add user and listing associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520223022) do
+ActiveRecord::Schema.define(version: 20140522210148) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140520223022) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "models", force: true do |t|
